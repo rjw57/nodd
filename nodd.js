8cb48c2fce69bea0c2a2122dc0dd185406c560c0
Rich Wareham <rjw57@cam.ac.uk>
2015-01-16 18:13:46 +0000

    s/noddy/nodd/

--- nodd.js
+++ nodd.js
@@ -16,28 +16,28 @@ var world_texture = THREE.ImageUtils.loadTexture("tex/world-map-1.jpg");
 world_texture.wrapS = THREE.RepeatWrapping;
 
 // Create a single plane which covers the NODDY logo area
-var noddy_geom = new THREE.PlaneBufferGeometry(2, 2.0/3.0);
-//var noddy_material = new THREE.MeshNormalMaterial();
-var noddy_material = new THREE.ShaderMaterial({
-  vertexShader: document.getElementById('noddyVS').textContent,
-  fragmentShader: document.getElementById('noddyFS').textContent,
+var nodd_geom = new THREE.PlaneBufferGeometry(2, 2.0/3.0);
+//var nodd_material = new THREE.MeshNormalMaterial();
+var nodd_material = new THREE.ShaderMaterial({
+  vertexShader: document.getElementById('noddVS').textContent,
+  fragmentShader: document.getElementById('noddFS').textContent,
   uniforms: {
     time: { type: "f", value: 0.0 },
     mapTexture: { type: "t", value: world_texture },
   },
 });
-var noddy_mesh = new THREE.Mesh(noddy_geom, noddy_material);
-noddy_mesh.rotation.y = Math.PI; // To show the visible face to the camera
-noddy_mesh.rotation.z = Math.PI;
+var nodd_mesh = new THREE.Mesh(nodd_geom, nodd_material);
+nodd_mesh.rotation.y = Math.PI; // To show the visible face to the camera
+nodd_mesh.rotation.z = Math.PI;
 
-scene.add(noddy_mesh);
+scene.add(nodd_mesh);
 
 var angularSpeed = 0.2;
 var lastTime = 0, startTime = (new Date()).getTime();
 function render() {
   // update
   var time = (new Date()).getTime() - startTime;
-  noddy_material.uniforms.time.value = time * 7e-5;
+  nodd_material.uniforms.time.value = time * 7e-5;
   var timeDiff = time - lastTime;
   var angleChange = angularSpeed * timeDiff * 2 * Math.PI / 1000;
   lastTime = time;
